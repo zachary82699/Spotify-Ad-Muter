97a96cd428370669ff6e3c75a2eb1260ad44bb64
Zach <51203277+zachary82699@users.noreply.github.com>
2021-03-05 20:25:15 +0000

    Update install requirements.bat
    updating the .bat file to 'pip install' from inside of the .bat 😄

--- install requirements.bat
+++ install requirements.bat
@@ -1,2 +1,4 @@
 python -m pip install --trusted-host pypi.python.org --trusted-host files.pythonhosted.org --trusted-host pypi.org --upgrade pip
-pip install -r requirements.txt
+pip install psutil==5.8.0
+pip install pywin32
+pip install pycaw==20181226
